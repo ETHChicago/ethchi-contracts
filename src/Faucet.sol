a02184afe8587f55f0319e0453470f6386281cab
Thomas <trbiv@proton.me>
2023-08-07 18:38:38 -0500

    initial faucet contract with tests

---
+++ src/Faucet.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Faucet {
+    address public owner;
+    uint public claimAmount = 1 ether;
+
+    // keep track of addresses that have already claimed
+    mapping(address => bool) public claimed;
+
+    constructor() payable {
+        owner = msg.sender; 
+    }
+
+    modifier onlyOwner {
+       require(msg.sender == owner, "Only the owner can call this function");
+       _;
+    }
+
+    function setOwner(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+
+    function setClaimAmount(uint newClaimAmount) public onlyOwner {
+        claimAmount = newClaimAmount;
+    }
+
+    function donate() public payable {}
+
+    function claimTokens(address payable claimer) public payable {
+        require(address(this).balance > claimAmount, "Not enought funds in the faucet. Please donate");
+        require(claimed[msg.sender] == false, "User has already claimed tokens.");
+
+        claimer.transfer(claimAmount);
+
+        // can only request 
+        claimed[msg.sender] = true;
+    }
+
+}
---
+++ test/Faucet.t.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Faucet.sol";
+
+contract FaucetTest is Test {
+
+    function test_ClaimTokens() public {
+        Faucet faucet = new Faucet{value: 10 ether}();
+        uint claimAmount = faucet.claimAmount();
+        address payable user = payable(address(0x111));
+
+        assertFalse(faucet.claimed(address(this)), "address has not yet claimed any ether");
+
+        faucet.claimTokens{value: 0}(user);    
+
+        assert(user.balance == claimAmount);
+        assertTrue(faucet.claimed(address(this)), "user has claimed tokens");
+    }
+
+    function testFail_ClaimNotEnoughtFunds() public {
+        Faucet faucet = new Faucet{value: 0}();
+        address payable user = payable(address(0x112));
+
+        faucet.claimTokens{value: 0}(user);
+    }
+
+    function testFail_DuplicateClaim() public {
+        Faucet faucet = new Faucet{value: 10 ether}();
+        address payable user = payable(address(0x113));
+
+        faucet.claimTokens{value: 0}(user);
+        faucet.claimTokens{value: 0}(user);
+    }
+
+    function test_SetOwner() public {
+        Faucet faucet = new Faucet{value: 10 ether}();
+        address payable user = payable(address(0x113));
+
+        address owner = faucet.owner();
+        assertTrue(owner == address(this), "test contract is initially owner");
+
+        faucet.setOwner(user);
+        owner = faucet.owner();
+        assertTrue(owner == user, "other user should now be owner");
+    }
+
+    function testFail_SetOwnerNonOwner() public {
+        Faucet faucet = new Faucet{value: 10 ether}();
+        address payable user = payable(address(0x113));
+
+        address owner = faucet.owner();
+        assertTrue(owner == address(this), "test contract is initially owner");
+
+        // call setOwner as other user 
+        vm.startPrank(user);
+        faucet.setOwner(user);
+        vm.stopPrank();
+    }
+
+    function test_SetClaimAmount() public {
+        Faucet faucet = new Faucet{value: 10 ether}();
+        uint initialClaimAmount = faucet.claimAmount();
+        uint newClaimAmount = 2 ether;
+
+        assertFalse(initialClaimAmount == newClaimAmount, "claims amounts must be different");
+
+        faucet.setClaimAmount(newClaimAmount);
+
+        assertTrue(newClaimAmount == faucet.claimAmount(), "claim amount should be updated to the new amount");
+    }
+
+    function testFail_SetClaimAmountNonOwner() public {
+        Faucet faucet = new Faucet{value: 10 ether}();
+        uint initialClaimAmount = faucet.claimAmount();
+        uint newClaimAmount = 2 ether;
+
+        assertFalse(initialClaimAmount == newClaimAmount, "claims amounts must be different");
+
+        address payable user = payable(address(0x114));
+        vm.startPrank(user);
+        faucet.setClaimAmount(newClaimAmount);
+    }
+
+    function test_Donate() public {
+        Faucet faucet = new Faucet{value: 10 ether}();
+        faucet.donate{value: 1 ether}();
+        assertTrue(address(faucet).balance == 11 ether);
+    }
+
+}
