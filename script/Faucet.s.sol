f79be6ec3b9f1706ca5df8dce6d750a34bea26e9
Thomas <trbiv@proton.me>
2023-08-07 19:13:49 -0500

    deployed

--- script/Faucet.s.sol
+++ script/Faucet.s.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/Faucet.sol";
 
 contract FaucetDeploy is Script {
-    function setUp() public {}
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
-    function run() public {
-        vm.broadcast();
+        Faucet faucet = new Faucet();
+
+        vm.stopBroadcast();
     }
 }
