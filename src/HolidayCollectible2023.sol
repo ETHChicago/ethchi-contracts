25227946f4765cf6afbf82a9eb4c7ecf7b67e176
Thomas Butler <trbiv@proton.me>
2023-12-07 17:22:40 -0600

    tweaks

--- src/HolidayCollectible2023.sol
+++ src/HolidayCollectible2023.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract MyToken is ERC721, ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
 
     constructor(address initialOwner)
-        ERC721("MyToken", "MTK")
+        ERC721("ETHCHI Holiday Collectible 2023", "ETHCHI_XMAS_2023")
         Ownable(initialOwner)
     {}
 
